3ad028ef8b0ac94fd2fc12c7d3b1ffad3c8c0f3d
Assistencia SMUP <tec@smup.site>
2019-02-09 12:04:57 -0200

    tudo ok

---
+++ db.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "myusername",
+  password: "mypassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  /*Create a database named "mydb":*/
+  con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+});
+
